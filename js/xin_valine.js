2397d20da42ecbe7ebb6d4507093e91c74033cee
wangjiabin <wangjiabin@digitalgene.ai>
2023-02-07 14:44:17 +0800

    Site updated: 2023-02-07 14:44:17

--- js/xin_valine.js
+++ js/xin_valine.js
@@ -50,21 +50,21 @@
 var SCKEY_Server="SCT195211TOjCqd93TcS4uFQRwRaeIVhsl";
 var ValineButton=document.getElementsByClassName("vsubmit vbtn")[0]; 
 function send_valine_Server(){ 
-    var text="desp="; 
-    var pagename=document.title;
-    var wz=pagename.indexOf('|'); 
-    var res=pagename.substring(0,wz); 
-    var pageurl=document.URL; 
-    var ptime=new Date(); 
-    var vnick=document.getElementsByClassName("vnick vinput")[0].value; 
-    var vmail=document.getElementsByClassName("vmail vinput")[0].value; 
-    var veditor=document.getElementsByClassName("veditor vinput")[0].value; 
-    var data=text+"昵称: " + vnick +"\n\n"+ "邮箱: " + vmail +"\n\n"+ "当前页面: " + res + "\n\n" + "评论内容: " + veditor + "\n\n" + "跳转链接: " + pageurl + "\n\n" + "评论时间: " + ptime.toLocaleString();
-    var httpRequest = new XMLHttpRequest();//第一步：创建需要的对象 
-    var title1="text=博客评论: " + res + '--' + veditor;
-    httpRequest.open('POST', 'https://sctapi.ftqq.com/'+SCKEY_Server+'.send', true); //第二步：打开连接 
-    httpRequest.setRequestHeader("Content-type","application/x-www-form-urlencoded");//设置请求头 注：post方式必须设置请求头（在建立连接后设置请求头） 
-    httpRequest.send(title1+"&"+data);//发送请求 将情头体写在send中 
-    // console.log("有新评论啦22222"); 
+    // var text="desp="; 
+    // var pagename=document.title;
+    // var wz=pagename.indexOf('|'); 
+    // var res=pagename.substring(0,wz); 
+    // var pageurl=document.URL; 
+    // var ptime=new Date(); 
+    // var vnick=document.getElementsByClassName("vnick vinput")[0].value; 
+    // var vmail=document.getElementsByClassName("vmail vinput")[0].value; 
+    // var veditor=document.getElementsByClassName("veditor vinput")[0].value; 
+    // var data=text+"昵称: " + vnick +"\n\n"+ "邮箱: " + vmail +"\n\n"+ "当前页面: " + res + "\n\n" + "评论内容: " + veditor + "\n\n" + "跳转链接: " + pageurl + "\n\n" + "评论时间: " + ptime.toLocaleString();
+    // var httpRequest = new XMLHttpRequest();//第一步：创建需要的对象 
+    // var title1="text=博客评论: " + res + '--' + veditor;
+    // httpRequest.open('POST', 'https://sctapi.ftqq.com/'+SCKEY_Server+'.send', true); //第二步：打开连接 
+    // httpRequest.setRequestHeader("Content-type","application/x-www-form-urlencoded");//设置请求头 注：post方式必须设置请求头（在建立连接后设置请求头） 
+    // httpRequest.send(title1+"&"+data);//发送请求 将情头体写在send中 
+    console.log("有新评论啦22222"); 
 }; 
 ValineButton.onclick=send_valine_Server();
